82db99092fda45cc18ad4c952c435c0d9d60e93a
Prakash Gurung <prakashgrg4400@gmail.com>
2024-05-04 07:42:14 +0545

    handeling form validation for register form

--- js/index.js
+++ js/index.js
@@ -49,3 +49,48 @@ function handleRegisterAndSignin(setActive, setPassive, btnActive, btnPassive) {
   passive.style.display = "none";
   btn2.setAttribute("class", "");
 }
+
+function handleSubmit(event) {
+  event.preventDefault();
+  //!==> We can select a form directly using "document.forms" , which will give us the a list of forms present in our html file inside a "HTMLCollection" . Now if you give "name" attribute to each form than we can directly select each form as shown below by directly writing the value present inside "name" attribut with the help of "document.forms.nae_attribute_value_here" .   The elements gives us all the "html element" present inside
+  let registerForm = document.forms.registerForm;
+  let elements = document.forms.registerForm.elements;
+
+  let name = elements.name;
+  let nameMessage = document.getElementById("name-message");
+
+  let email = elements.email;
+  let emailMessage = document.getElementById("email-message");
+
+  let password = elements.password;
+  let passwordMessage = document.getElementById("password-message");
+
+  if (name.value.trim().length == 0) {
+    name.style.border = "1px solid red";
+    nameMessage.textContent = "Name is required !";
+    nameMessage.style.color = "red";
+    nameMessage.style.display = "block";
+  } else if (email.value.trim().length == 0) {
+    email.style.border = "1px solid red";
+    emailMessage.style.color = "red";
+    emailMessage.textContent = "Email required !";
+    emailMessage.style.display = "block";
+  } else if (password.value.trim().length == 0) {
+    password.style.border = "1px solid red";
+    passwordMessage.style.color = "red";
+    passwordMessage.textContent = "Password required !";
+    passwordMessage.style.display = "block";
+  } else {
+    console.log(name.value);
+    console.log(email.value);
+    console.log(password.value);
+  }
+}
+
+function handleInput(inputElement, errorMessageId) {
+  let errorMessage = document.getElementById(errorMessageId);
+  inputElement.style.border = "1px solid grey";
+  errorMessage.style.textContent = "";
+  errorMessage.style.color = "black";
+  errorMessage.style.display = "none";
+}
